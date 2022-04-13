f104a2826c7649ed73eb2eca84ff74798b947671
Lean9090P <l.pino06@ufromail.cl>
2022-04-13 10:58:47 -0400

    Commit 4 Leandro

--- src/main/java/Sismos.java
+++ src/main/java/Sismos.java
@@ -1,33 +1,35 @@
 import java.util.*;
 
 public class Sismos {
-
     public static void main(String[] args) {
-        GenerarMatriz();
+
         Menu();
-    }
-    public static int [][] GenerarMatriz(){
-        Scanner teclado = new Scanner(System.in);
 
-        System.out.print("Introduce dias: ");
-        int dias = teclado.nextInt();
-        while (dias <2 || dias >31) {
-            System.out.print("Ingrese otro numero valido: ");
-            dias = teclado.nextInt();
-        }
-        System.out.println("Dia ingresado ="+dias);
-        int matriz[][] = new int[dias][24];
-        for (int i = 1; i < matriz.length; i++) {
-            System.out.println();
-            System.out.print(" [" + i + "] ");
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = (int) (Math.random() * (10 - 1) + 1);
-                System.out.print(matriz[i][j] + " ");
-            }
-        }
-        return matriz;
+
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     public static void Menu() {
 
         Scanner sn = new Scanner(System.in);
